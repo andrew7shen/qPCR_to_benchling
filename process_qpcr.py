67b1e2cd92da7cbbee1a7ddf46dac9ab449fd291
Andrew Shen <andrew7shen@gmail.com>
2022-06-23 13:57:04 -0700

    Working with get_seq_source stack trace

--- process_qpcr.py
+++ process_qpcr.py
@@ -13,31 +13,32 @@ from benchling_sgmo import package_data
 # Assign input files
 qpcr_in = sys.argv[1]
 
-# Read in input qPCR file and convert to proper dictionary format
+# Read in input qPCR file and convert to proper list of dictionaries format
 df = pd.read_csv(qpcr_in)
 df = df.reset_index()
-#print(df["Sample"])
-#print(df)
-qpcr_dict = {}
+qpcr_dict_list = []
 for i, row in df.iterrows():
-    qpcr_dict[row["Sample"]] = [row["ZFC ID"], row["Target"], row["Well"], row["Data Set"], row["Content"], row["Type"], row["Cq"], row["Starting Quantity (SQ)"], row["Control"], row["cDNA"], row["fn"], row["sample excluded"], row["SQ"], row["Absolute Quantity"], row["Absolute Quantity SD"], row["Mean Cq"], row["Cq SD"], row["Efficiency"], row["Relative Quantity"], row["Relative Quantity SD"], row["Normalization Factor"], row["Normalization Factor SD"], row["Expression"], row["Expression SD"], row["Dose"]]
-#for k,v in qpcr_dict.items():
-#    print(v)
+    currDict = row.to_dict()
+    qpcr_dict_list.append(currDict)
+
+#qpcr_dict = {}
+#for i, row in df.iterrows():
+#    qpcr_dict[row["Sample"]] = [row["ZFC ID"], row["Target"], row["Well"], row["Data Set"], row["Content"], row["Type"], row["Cq"], row["Starting Quantity (SQ)"], row["Control"], row["cDNA"], row["fn"], row["sample excluded"], row["SQ"], row["Absolute Quantity"], row["Absolute Quantity SD"], row["Mean Cq"], row["Cq SD"], row["Efficiency"], row["Relative Quantity"], row["Relative Quantity SD"], row["Normalization Factor"], row["Normalization Factor SD"], row["Expression"], row["Expression SD"], row["Dose"]]
+
 
 # Connect to Benchling
-#seq_source = get_seq_source()
-#print(seq_source)
-def connect_to_bench():
-    # Calls the get_seq_source() function within the get_sequence_source.py file
-    seq_source = get_sequence_source.get_seq_source()
-    return seq_source
-#    seq_source = get_seq_source(is_test=True)
-bench = connect_to_bench()
-#bench =
+# Calls the get_seq_source() function within the get_sequence_source.py file
+is_test = True
+seq_source = get_sequence_source.get_seq_source(is_test=is_test)
+bench = seq_source
 
 # Package data
 schema = "qPCR Result"
-bench_list = package_data.bulk_package_data(data=qpcr_dict, schema_name=schema, api_client=bench)
+#source = get_sequence_source.get_sequence_source_benchling()
+#bench1 = source.connection.benchling
+#print(bench1)
+bench_list = package_data.bulk_package_data(data=qpcr_dict_list, schema_name=schema, api_client=bench)
+
 # Upload to Benchling
 print(bench_list)
 
